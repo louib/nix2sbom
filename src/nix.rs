571048ab02267f9153dc42196f7f87691c75acf9
louib <code@louib.net>
2023-08-13 22:12:58 -0400

    feat: parse the nix store metadata

--- src/nix.rs
+++ src/nix.rs
@@ -55,3 +55,74 @@ pub fn get_dependencies(path: &str) -> Vec<String> {
     // TODO nix-store -qR /an/executable/path
     vec![]
 }
+
+// Get the derivation path associated with a store object
+pub fn get_derivation_path(store_path: &str) -> String {
+    // TODO nix-store -qd store_path
+    "".to_string()
+}
+pub fn get_packages() -> Result<HashMap<String, PackageMeta>, String> {
+    // There is currently no way with Nix to generate the meta information
+    // only for a single derivation. We need to generate the meta for
+    // all the derivations in the store and then extract the information
+    // we want from the global meta database.
+    let output = Command::new("nix-env")
+        .arg("-q")
+        .arg("-a")
+        .arg("--meta")
+        .arg("--json")
+        .arg("'.*'")
+        .output()
+        .map_err(|e| e.to_string())?;
+
+    let packages: HashMap<String, PackageMeta> =
+        serde_json::from_slice(&output.stdout).map_err(|e| e.to_string())?;
+    Ok(packages)
+}
+
+#[derive(Debug)]
+#[derive(Serialize)]
+#[derive(Deserialize)]
+pub struct Meta {
+    pub packages: HashMap<String, PackageMeta>,
+}
+
+#[derive(Debug)]
+#[derive(Serialize)]
+#[derive(Deserialize)]
+pub struct Package {
+    // name of the derivation
+    name: String,
+
+    // package name
+    pname: String,
+
+    // package version
+    version: String,
+
+    // name of the system for which this package was built
+    system: String,
+
+    // name of the output
+    #[serde(rename = "outputName")]
+    output_name: String,
+}
+
+#[derive(Debug)]
+#[derive(Serialize)]
+#[derive(Deserialize)]
+pub struct PackageMeta {
+    available: Option<bool>,
+
+    broken: Option<bool>,
+
+    insecure: Option<bool>,
+
+    description: Option<String>,
+
+    unfree: Option<bool>,
+
+    unsupported: Option<bool>,
+
+    homepage: Option<String>,
+}
