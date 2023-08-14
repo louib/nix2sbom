ef5428c4014bd77d874d5c9d811a587b1ddd90c6
louib <code@louib.net>
2023-08-13 20:07:44 -0400

    feat: add basic clap interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// TODO tune built-in attributes
+// From https://doc.rust-lang.org/reference/items/modules.html#attributes-on-modules
+// The built-in attributes that have meaning on a module are cfg, deprecated, doc,
+// the lint check attributes, path, and no_implicit_prelude.
+// Modules also accept macro attributes.
+
+#[macro_use]
+extern crate clap;
+
+use clap::{AppSettings, Parser, Subcommand};
+
+/// nix2sbom extracts the SBOM (Software Bill of Materials) from a Nix derivation
+#[derive(Parser)]
+#[clap(name = "nix2sbom")]
+#[clap(version = env!("CARGO_PKG_VERSION"))]
+#[clap(about = "nix2sbom extracts the SBOM (Software Bill of Materials) from a Nix derivation", long_about = None)]
+struct NixToSBOM {}
+
 fn main() {
     println!("Hello, world!");
 }
