e0182e206c9e5bb782fcf66aa93d1203e6b61216
louib <code@louib.net>
2023-08-17 23:19:38 -0400

    feat: parse format from extension

--- src/sbom.rs
+++ src/sbom.rs
@@ -3,6 +3,18 @@ enum Format {
     CycloneDX,
 }
 
+impl Format {
+    pub fn from_file_extension(file_path: &str) -> Option<Format> {
+        if file_path.ends_with(".spdx") {
+            return Some(Format::SPDX);
+        }
+        if file_path.ends_with(".cdx") {
+            return Some(Format::CycloneDX);
+        }
+        None
+    }
+}
+
 enum SerializationFormat {
     JSON,
     YAML,
