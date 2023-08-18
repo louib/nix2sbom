959de8939e0da62df6017a196c553275c9747b94
louib <code@louib.net>
2023-08-17 23:54:03 -0400

    feat: populate the tools metadata

--- src/cyclone_dx.rs
+++ src/cyclone_dx.rs
@@ -3,13 +3,20 @@ use std::time::SystemTime;
 use chrono::{DateTime, Utc};
 use serde::{de::Deserialize, ser::Serialize};
 
-use serde_cyclonedx::cyclonedx::v_1_4::{CycloneDxBuilder, Metadata};
+use serde_cyclonedx::cyclonedx::v_1_4::{CycloneDxBuilder, Metadata, ToolBuilder};
 
 pub fn dump() -> String {
     let mut metadata = Metadata::default();
     let now = SystemTime::now();
     let now: DateTime<Utc> = now.into();
     metadata.timestamp = Some(now.to_rfc3339());
+
+    metadata.tools = Some(vec![ToolBuilder::default()
+        .vendor("louib".to_string())
+        .name("nix2sbom".to_string())
+        .version(env!("CARGO_PKG_VERSION"))
+        .build()
+        .unwrap()]);
 
     let cyclonedx = CycloneDxBuilder::default()
         .bom_format("CycloneDX")
